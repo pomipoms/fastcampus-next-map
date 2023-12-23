91281e4918217000556c70e5949d4144eb901cde
na woo <flyingdog@nas-MacBook-Pro.local>
2023-12-23 15:14:57 +0900

    added map

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,39 +1,12 @@
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
+import Map from '@/components/Map';
 import Link from 'next/link';
-const inter = Inter({ subsets: ['latin'] })
+
 
 export default function Home() {
+
   return (
-    <div>
-          <h1 className='font-bold'>Map Index Page</h1>
-
-<ul className='px-4 py-2'>
-  <li>
-    <Link href="/stores">Restaurant List</Link>
-  </li>
-  <li>
-    <Link href="/stores/new">Create a new restaurant</Link>
-  </li>
-  <li>
-    <Link href="/stores/1">Restaurant Detail</Link>
-  </li>
-  <li>
-    <Link href="/stores/1/edit">Edit Restaurant Detail</Link>
-  </li>
-  <li>
-    <Link href="/users/login">Login Page</Link>
-  </li>
-  <li>
-    <Link href="/users/mypage">My Page</Link>
-  </li>
-  <li>
-    <Link href="/users/likes">Likes </Link>
-  </li>
-
-</ul>
-    </div>
-
-
-  )
+    <>
+    <Map />
+    </>
+  );
 }
